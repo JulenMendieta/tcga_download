caaa349d074a67c2d6d4eeb027640c99a2eb0898
dmontaner <david.montaner@gmail.com>
2015-01-14 18:58:13 +0100

    first commit

---
+++ scripts/download_clinical_data.r
@@ -0,0 +1,205 @@
+##download_clinical_data.r
+##2015-01-13 dmontaner@cipf.es
+##Collecting data from TCGA
+
+## The scripts uses TCGA DCC Web Services to find out all CLINICAL data.
+
+## Find the platform here:
+## http://tcga-data.nci.nih.gov/tcgadccws/GetHTML?query=Platform
+## "bio" seems to be the appropriated one
+
+## grep -B 1 -A 2 E6B279BC-97A1-4B9A-9632-06326C418E1C nationwidechildrens.org_biospecimen_sample_coad.txt 
+
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ##"R version 3.1.2 (2014-10-31)"
+library (RCurl); packageDescription ("RCurl", fields = "Version") #"1.95-4.3"
+library (XML); packageDescription ("XML", fields = "Version") #"3.98-1.1"
+#help (package = XML)
+#help (package = RCurl)
+
+try (source (".job.r")); try (.job)
+
+options (width = 170)
+#options (width = 1000)
+
+setwd (file.path (.job$dir$raw, "clinical"))
+
+################################################################################
+
+## DOWNLOAD DATA
+base   <- "https://tcga-data.nci.nih.gov"
+my.url <- "http://tcga-data.nci.nih.gov/tcgadccws/GetXML?query=Archive[Platform[@name=bio]][ArchiveType[@type=Level_2]][@deployStatus=Available][@isLatest=1]"
+
+char <- getURL (my.url)
+char
+
+mix <- xmlInternalTreeParse (char)
+class (mix)
+mix <- xmlChildren (xmlChildren (mix)[["httpQuery"]])[["queryResponse"]]
+class (mix)
+mix
+
+ns <- getNodeSet (mix, path = "class")
+class (ns)
+length (ns)
+ns
+
+datos <- xmlToDataFrame (ns, stringsAsFactors = FALSE)
+dim (datos)
+datos[1:3,]
+colnames (datos) <- paste0 ("V", 1:ncol (datos))
+colnames (datos)[c (1, 2, 3, 7)] <- c("date", "baseName", "location", "name")
+datos[1:3,]
+
+datos[,"fecha"] <- as.Date (datos[,"date"], "%m-%d-%Y")
+datos[,c ("date", "fecha")]
+
+datos[,"url"] <- paste0 (base, datos[,3])
+
+datos[,"tag"] <- sapply (strsplit (datos[,"baseName"], split = "_"), function (x) x[2])
+table (duplicated (datos[,"tag"]))
+rownames (datos) <- datos[,"tag"]
+datos[1:3,]
+
+system.time ({
+    for (url in datos[,"url"]) {
+        filename <- basename (url)
+        download.file (url = url, destfile = filename, method = "curl")
+        untar (tarfile = filename, compressed = TRUE)    
+    }
+})
+
+################################################################################
+
+## FIND UUID
+ficheros <- dir (pattern = "biospecimen_shipment_portion", recursive= TRUE)
+length (ficheros)
+
+datos <- NULL
+for (fi in ficheros) {
+    print (fi)
+    nombres <- unlist (strsplit (readLines (fi)[1], split = "\t"))
+    #print (nombres)
+    datos0 <- read.table (fi, header = FALSE, sep = "\t", quote = "", col.names = nombres, as.is = TRUE, na.strings = "[Not Available]", skip = 2)
+    datos0[,"file"] <- fi
+    print (dim (datos0))
+    try (datos <- rbind (datos, datos0))
+}
+dim (datos)
+
+datos[,"tag"] <- sapply (strsplit (sapply (strsplit (datos[,"file"], split = "_"), function (x) x[2]), split = "\\."), function (x) x[1])
+table (datos[,"tag"], exclude = NULL)
+
+if (any (duplicated (datos[,"bcr_shipment_portion_uuid"]))) stop ("DUPLICATED") ## OK NO DUPS
+
+##protein data
+load (file.path (.job$dir$proces, "prot_exp.RData"))
+dim (prot.exp)
+
+prot.ids <- colnames (prot.exp)
+prot.ids[1:3]
+
+table (datos[,"bcr_shipment_portion_uuid"] %in% prot.ids)
+table (prot.ids %in% datos[,"bcr_shipment_portion_uuid"]) #deberian esta todos ???
+table (tolower (prot.ids) %in% tolower (datos[,"bcr_shipment_portion_uuid"])) #deberian esta todos ???
+setdiff (tolower (prot.ids), tolower (datos[,"bcr_shipment_portion_uuid"])) #deberian esta todos ???
+
+malos <- !tolower (prot.ids) %in% tolower (datos[,"bcr_shipment_portion_uuid"])
+table (malos)
+table (prot.exp.info[malos, "tag"])
+
+datos[,"has.prot"] <- tolower (datos[,"bcr_shipment_portion_uuid"]) %in% tolower (prot.ids)
+table (datos[,"has.prot"])
+table (datos[,"tag"], datos[,"has.prot"])
+
+################################################################################
+
+## FIND CLASSES
+ficheros <- dir (pattern = "biospecimen_sample", recursive = TRUE)
+length (ficheros)
+ficheros
+
+##apagno un fichero que esta mal
+li <- readLines ("nationwidechildrens.org_COAD.bio.Level_2.0.17.0/nationwidechildrens.org_biospecimen_sample_coad.txt")
+li[1:10]
+lisp <- strsplit (li, split = "\t")
+lon <- sapply (lisp, length)
+table (lon)
+which (lon == 20)
+
+li[376] <- sub ("E6B279BC-97A1-4B9A-9632-06326C418E1C", "[Not Available]\tE6B279BC-97A1-4B9A-9632-06326C418E1C", li[376])
+writeLines (li, con = "nationwidechildrens.org_COAD.bio.Level_2.0.17.0/nationwidechildrens.org_biospecimen_sample_coad.txt")
+
+datos.li <- list ()
+for (fi in ficheros) {
+    cat ("\n======================== ", fi, " ==============================\n")
+    tag <- sub (".txt", "", rev (unlist (strsplit (fi, split = "_")))[1])
+    ##
+    nombres <- unlist (strsplit (readLines (fi)[1], split = "\t"))
+    datos0 <- read.table (fi, header = FALSE, sep = "\t", quote = "", col.names = nombres, as.is = TRUE, na.strings = "[Not Available]", skip = 2)
+    datos0[,"tag"] <- tag
+    datos.li[[fi]] <- datos0
+}
+
+t (sapply (datos.li, dim))
+colSums (t (sapply (datos.li, dim)))
+
+unicol <- unique (unlist (sapply (datos.li, colnames)))
+datos1 <- NULL
+for (fi in ficheros) {
+    cat ("\n======================== ", fi, " ==============================\n")
+    datos0 <- datos.li[[fi]]
+    faltan <- setdiff (unicol, colnames (datos0))
+    datos0[,faltan] <- NA
+    datos0 <- datos0[,unicol]
+    datos1 <- rbind (datos1, datos0)
+}
+dim (datos1)
+
+sapply (datos1, class)
+
+datos1[1:3,]
+
+table (datos1[,"tag"], exclude = NULL)
+table (datos1[,"sample_type"], exclude = NULL)
+
+################################################################################
+
+## COMBINE
+dim (datos)
+dim (datos1)
+
+datos1[1:3,]
+datos[1:3,]
+
+datos1[1:3, c ("bcr_sample_barcode", "bcr_sample_uuid", "sample_type")]
+datos [1:3, c ("bcr_sample_barcode", "shipment_portion_bcr_aliquot_barcode", "bcr_shipment_portion_uuid")]
+
+intersect (colnames (datos), colnames (datos1))
+
+table (datos[,"bcr_sample_barcode"] %in% datos1[,"bcr_sample_barcode"])
+setdiff (datos[,"bcr_sample_barcode"], datos1[,"bcr_sample_barcode"])
+table (datos1[,"bcr_sample_barcode"] %in% datos[,"bcr_sample_barcode"])
+
+combi <- merge (datos, datos1, by = "bcr_sample_barcode", all = TRUE)
+dim (combi)
+combi[1:3,]
+
+table (combi$has.prot, exclude = NULL)
+combi[is.na (combi$has.prot), "has.prot"] <- FALSE
+
+table (tolower (combi[,"tag.x"]) == tolower (combi[,"tag.y"]), exclude = NULL)
+igual <- tolower (combi[,"tag.x"]) == tolower (combi[,"tag.y"])
+combi[which (!igual), c("tag.x", "tag.y")]
+
+
+table (combi[combi$has.prot, c("sample_type", "tag.x")])
+table (combi[combi$has.prot, c("sample_type", "tag.y")])
+
+###EXIT
+warnings ()
+sessionInfo ()
+q ("no")
---
+++ scripts/download_protein_expression.r
@@ -0,0 +1,220 @@
+##download_protein_expression.r
+##2015-01-13 dmontaner@cipf.es
+##Collecting data from TCGA
+
+## The scripts uses TCGA DCC Web Services to find out all the PROTEIN EXPRESSION data.
+
+## Batch effect is very strong.
+## Do just comparisons whiting each disease.
+
+## Find the platform here:
+## http://tcga-data.nci.nih.gov/tcgadccws/GetHTML?query=Platform
+
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ##"R version 3.1.2 (2014-10-31)"
+library (RCurl); packageDescription ("RCurl", fields = "Version") #"1.95-4.3"
+library (XML); packageDescription ("XML", fields = "Version") #"3.98-1.1"
+#help (package = XML)
+#help (package = RCurl)
+
+try (source (".job.r")); try (.job)
+
+options (width = 170)
+#options (width = 1000)
+
+setwd (file.path (.job$dir$raw, "protein_exp"))
+
+################################################################################
+
+## DOWNLOAD DATA
+
+base   <- "https://tcga-data.nci.nih.gov"
+my.url <- "http://tcga-data.nci.nih.gov/tcgadccws/GetXML?query=Archive[Platform[@name=MDA_RPPA_Core]][ArchiveType[@type=Level_3]][@deployStatus=Available][@isLatest=1]"
+
+char <- getURL (my.url)
+char
+
+mix <- xmlInternalTreeParse (char)
+class (mix)
+mix <- xmlChildren (xmlChildren (mix)[["httpQuery"]])[["queryResponse"]]
+class (mix)
+mix
+
+ns <- getNodeSet (mix, path = "class")
+class (ns)
+ns
+
+datos <- xmlToDataFrame (ns, stringsAsFactors = FALSE)
+dim (datos)
+colnames (datos) <- paste0 ("V", 1:ncol (datos))
+colnames (datos)[c (1, 3, 7)] <- c("date", "location", "name")
+datos[1:3, 1:8]
+
+datos[,"fecha"] <- as.Date (datos[,"date"], "%m-%d-%Y")
+datos[,c ("date", "fecha")]
+
+datos[,"url"] <- paste0 (base, datos[,3])
+##datos[,"gzfile"] <- basename (datos[,3])
+##datos[,"gzfolder"]  <- sub (".tar.gz", "", datos[,"gzfile"])
+
+rownames (datos) <- datos[,"name"]
+
+datos[1:3,]
+
+system.time ({
+    for (url in datos[,"url"]) {
+        filename <- basename (url)
+        download.file (url = url, destfile = filename, method = "curl")
+        untar (tarfile = filename, compressed = TRUE)    
+    }
+})
+
+################################################################################
+
+## SOME CHECKS
+length (dir (recursive= TRUE))
+
+ficheros <- dir (pattern = "protein_expression", recursive= TRUE)
+length (ficheros)
+ficheros[1:3]
+
+li <- strsplit (ficheros, ".protein_expression.Level_3.")
+table (sapply (li, length))  ##OK all 2
+uuid <- sapply (li, function (x) x[2])
+uuid <- sub (".txt", "", uuid)
+length (uuid)
+
+folder <- dirname (ficheros)
+
+fi <- sub ("mdanderson.org_", "", ficheros)
+li <- strsplit (fi, "\\.")
+table (sapply (li, length))  ##OK all the same
+tag <- sapply (li, function (x) x[1])
+length (tag)
+
+finfo <- as.data.frame (list (fichero = ficheros, uuid = uuid, folder = folder, tag = tag), stringsAsFactors= FALSE)
+class (finfo)
+dim (finfo)
+sapply (finfo, class)
+table (finfo[,"tag"])
+
+finfo[,"uuid.infile"] <- NA
+system.time ({
+    for (i in 1:nrow (finfo)) {
+        finfo[i, "uuid.infile"] <- readLines (con = finfo[i, "fichero"], n = 1)
+    }
+})
+finfo[,"uuid.infile"] <- sub ("Sample REF\t", "", finfo[,"uuid.infile"])
+table (finfo[,"uuid"] == finfo[,"uuid.infile"], exclude = NULL)  ## OK all the same
+
+if (any (finfo[,"uuid"] != finfo[,"uuid.infile"])) stop ("UUIDs do not match")
+
+summary (finfo)
+
+########################################
+
+## Some duplicated UUIDS ????
+dups <- duplicated (finfo[,"uuid"])
+table (dups)
+
+duplicados <- finfo[dups, "uuid"]
+finfo[,"is.dup"] <- finfo[,"uuid"] %in% duplicados
+table (finfo[,"is.dup"], exclude = NULL)
+
+table (table (finfo[,"uuid"], exclude = NULL), exclude = NULL)
+table (finfo[,"tag"], finfo[,"is.dup"])
+
+##sort by date and SERIAL INDEX
+table (finfo$folder %in% rownames (datos))  ## OK
+finfo[,"fecha"] <- datos[finfo$folder, "fecha"]
+orden <- order (finfo[,"fecha"], finfo[,"is.dup"], finfo[,"uuid"], finfo[,"folder"], decreasing = TRUE)
+finfo <- finfo[orden,]
+finfo[finfo$is.dup,][1:10,]
+
+malos <- finfo[finfo$is.dup,]
+orden <- order (malos[,"uuid"])
+malos <- malos[orden,]
+table (malos[,"tag"])
+malos[1:10,]
+tail (malos)
+
+dup <- duplicated (finfo[,"uuid"])
+table (dup)
+finfo <- finfo[!dup,]
+table (duplicated (finfo[,"uuid"]))
+     
+rownames (finfo) <- finfo[,"uuid"]
+
+################################################################################
+
+## READ DATA
+datos.li <- list ()
+system.time ({
+    for (i in 1:nrow (finfo)) {
+        datos.li[[finfo[i, "uuid"]]] <-
+            read.table (file = finfo[i, "fichero"], header = TRUE, sep = "\t", quote = "", skip = 1, colClasses = c ("character", "numeric"), row.names = 1)
+    }
+})
+length (datos.li)
+table (names (datos.li) == rownames (finfo))
+
+unique (sapply (datos.li, colnames)) ## OK all columns the same
+
+
+## MAKE MATRIX
+id.list <- lapply (datos.li, rownames)
+summary (sapply (id.list, length))
+summary (sapply (datos.li, nrow))
+
+ids <- sort (unique (unlist (id.list)))
+length (ids)
+ids
+
+prot.exp <- matrix (NA, nrow = length (ids), ncol = length (datos.li))
+dim (prot.exp)
+rownames (prot.exp) <- ids
+colnames (prot.exp) <- names (datos.li)
+prot.exp[1:3, 1:4]
+system.time ({
+    for (uid in names (datos.li)) {
+        mat <- datos.li[[uid]]
+        prot.exp[,uid] <- mat[ids,]
+    }
+})
+
+prot.exp[1:3, 1:4]
+
+table (colnames (prot.exp) == rownames (finfo))
+
+################################################################################
+
+## ## Some Plots
+## table (finfo$tag)
+
+## boxplot (prot.exp[,c(which (finfo$tag == "ACC"), which (finfo$tag == "BLCA"))])
+## abline (h = 0, col = "blue")
+## boxplot (prot.exp[,c(which (finfo$tag == "KIRP"), which (finfo$tag == "KIRC"))])
+## abline (h = 0, col = "blue")
+
+## orden <- order (finfo$tag)
+## boxplot (prot.exp[,orden])
+## abline (h = 0, col = "blue")
+
+## ran <- sample (colnames (prot.exp), size = 100)
+## boxplot (prot.exp[,ran])
+## abline (h = 0, col = "blue")
+
+################################################################################
+
+### SAVE
+prot.exp.info <- finfo
+table (colnames (prot.exp) == rownames (prot.exp.info))
+save (list = c("prot.exp", "prot.exp.info"), file = file.path (.job$dir$proces, "prot_exp.RData"))
+
+###EXIT
+warnings ()
+sessionInfo ()
+q ("no")
